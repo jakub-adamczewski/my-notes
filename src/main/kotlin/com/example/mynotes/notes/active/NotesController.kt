59d6cad3a1348b181b21760e9d8cbaf745f2d970
jakub <jakub.adamczewski@value-ag.de>
2021-11-08 10:00:16 +0100

    added context path

--- src/main/kotlin/com/example/mynotes/notes/active/NotesController.kt
+++ src/main/kotlin/com/example/mynotes/notes/active/NotesController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 
 @RestController
-@RequestMapping("api/notes")
+@RequestMapping("/notes")
 class NotesController(private val notesService: NotesService) {
 
     @GetMapping
